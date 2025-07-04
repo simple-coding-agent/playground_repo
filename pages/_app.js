0b920af7b017617e7a9ad8b1f1acfc450b517d09
simple-coding-agent <richard.mladek.jr@gmail.com>
2025-07-04 00:23:25 +0000

    Deleted all files in the repository as requested

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css';
-import '../styles/FlappyBird.module.css';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
